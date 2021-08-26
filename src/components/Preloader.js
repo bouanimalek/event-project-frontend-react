dc18f433772a064277b199295736dfaef7d25eac
dagbouj hatem <dagboujhatem@gmail.com>
2021-08-26 02:52:39 +0100

    change loader

--- src/components/Preloader.js
+++ src/components/Preloader.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Image } from '@themesberg/react-bootstrap';
 
 import ReactLogo from "../assets/img/technologies/react-logo-transparent.svg";
+import Loader from "react-loader-spinner";
 
 export default (props) => {
 
@@ -10,7 +11,24 @@ export default (props) => {
 
   return (
     <div className={`preloader bg-soft flex-column justify-content-center align-items-center ${show ? "" : "show"}`}>
-      <Image className="loader-element animate__animated animate__jackInTheBox" src={ReactLogo} height={40} />
+      {/* <Image className="loader-element animate__animated animate__jackInTheBox" src={ReactLogo} height={40} /> */}
+      
+      {/* exemple of loader with react-loader-spinner */}
+      
+      {/* <Loader type="Audio" color="#00BFFF" height={80} width={80} timeout={1000} />
+      <Loader type="BallTriangle" color="#00BFFF" height={80} width={80} timeout={1000} />
+      <Loader type="Bars" color="#00BFFF" height={80} width={80} timeout={1000} />
+      <Loader type="Circles" color="#00BFFF" height={80} width={80} timeout={1000} />
+      <Loader type="Grid" color="#00BFFF" height={80} width={80} timeout={1000} />
+      <Loader type="Hearts" color="#00BFFF" height={80} width={80} timeout={1000} />
+      <Loader type="Oval" color="#00BFFF" height={80} width={80} timeout={1000} />
+      <Loader type="Puff" color="#00BFFF" height={100} width={100} timeout={1000} />
+      <Loader type="Rings" color="#00BFFF" height={80} width={80} timeout={1000} />
+      <Loader type="TailSpin" color="#00BFFF" height={80} width={80} timeout={1000} />
+      <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} timeout={1000} /> */}
+
+      <Loader type="Oval" color="#00BFFF" height={80} width={80} timeout={1000} />
+
     </div>
   );
 };
